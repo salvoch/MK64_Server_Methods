0eb74c7d6f9144d60a90a2b3e985ba54d68e657d
salvoch <github@salvoch.com>
2022-08-03 17:30:07 -0600

    Add a data class that keeps track of constants throughout the program

---
+++ src/data/data.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace GameData
+{
+    public static class Constants
+    {
+        public static readonly string GAME_ID = "mk64";
+        public static readonly byte[] DEFAULT_VALUE = { 192, 39, 9 };
+        public static readonly string[] TRACK_NAMES =
+        {
+            "Luigi Raceway",
+            "Moo Moo Farm",
+            "Koopa Troopa Beach",
+            "Kalimari Desert",
+            "Toad's Turnpike",
+            "Frappe Snowland",
+            "Choco Mountain",
+            "Mario Raceway",
+            "Wario Stadium",
+            "Sherbet Land",
+            "Royal Raceway",
+            "Bowser's Castle",
+            "D.K's Jungle Parkway",
+            "Yoshi Valley",
+            "Banshee Boardwalk",
+            "Rainbow Road"
+        };
+        public static readonly string[] TRACK_SLUGS =
+        {
+            "luigiraceway",
+            "moomoofarm",
+            "koopatroopabeach",
+            "kalimaridesert",
+            "toadsturnpike",
+            "frappesnowland",
+            "chocomountain",
+            "marioraceway",
+            "wariostadium",
+            "sherbetland",
+            "royalraceway",
+            "bowserscastle",
+            "dksjungleparkway",
+            "yoshivalley",
+            "bansheeboardwalk",
+            "rainbowroad"
+        };
+        public static readonly string[] RACER_IDS =
+        {
+            "Mario",
+            "Luigi",
+            "Yoshi",
+            "Toad",
+            "DK",
+            "Wario",
+            "Peach",
+            "Bowser"
+        };
+    }
+}
