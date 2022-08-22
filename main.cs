868a3152034b1e2e215f3f9e443c7f58777bb68c
salvoch <github@salvoch.com>
2022-08-21 23:27:54 -0600

    Have the test main function play a system sound
    When the window pops up, have a system sound play to alert the user that their input is required.

--- main.cs
+++ main.cs
@@ -4,6 +4,7 @@ using GhostDownload;
 using Microsoft.VisualBasic;
 using System.Windows.Forms;
 using Win32Form1Namespace;
+using System.Media;
 
 namespace MainSpace
 {
@@ -14,6 +15,7 @@ namespace MainSpace
             //Create the input dialog box with the parameters below
             //string input = Interaction.InputBox("What is at the end of the rainbow?", "Riddle", "...", 10, 10);
             //System.Windows.Forms.ComboBox comboBox1 = new System.Windows.Forms.ComboBox();
+            SystemSounds.Exclamation.Play();
             Win32Form1 form1 = new Win32Form1();
             System.Windows.Forms.Application.Run(form1);
             Console.WriteLine(form1.type_result);
