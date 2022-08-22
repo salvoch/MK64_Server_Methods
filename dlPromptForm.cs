551f42ecab1543d6d7d70e64677a0142b51c312c
salvoch <github@salvoch.com>
2022-08-21 22:21:30 -0600

    Created first rough draft of ghost dl prompt
    A window pops up and asks user for flap or 3lap. right now this is just called from main and displays the choice.
    
    This will lead to being able to prompt the user upon ghost loading procedure

---
+++ dlPromptForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+/* Creates a form to prompt user for 3lap or flap ghost loading
+ * For the example code to this, see here:
+ * https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.combobox?view=windowsdesktop-6.0
+ * 
+ * If using .Net Core 6.0 and you cannot use System.Windows.Forms, see this:
+ * https://stackoverflow.com/a/70466224
+*/
+
+namespace Win32Form1Namespace
+{
+
+    public class Win32Form1 : System.Windows.Forms.Form
+    {
+        public string type_result = "NA";
+        private Button flapButton;
+        private Button tlapButton;
+        private Label label1;
+
+        public Win32Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        [System.STAThreadAttribute()]
+        public static void tMain()
+        {
+            System.Windows.Forms.Application.Run(new Win32Form1());
+        }
+
+        private void InitializeComponent()
+        {
+            //Fast lap button
+            this.flapButton = new Button();
+            this.flapButton.Location = new System.Drawing.Point(248, 32);
+            this.flapButton.Size = new System.Drawing.Size(400, 24);
+            this.flapButton.TabIndex = 1;
+            this.flapButton.Text = "Load Fast Lap Ghost";
+            this.flapButton.Click += new System.EventHandler(this.flapButton_Click);
+
+            //three lap button
+            this.tlapButton = new Button();
+            this.tlapButton.Location = new System.Drawing.Point(248, 64);
+            this.tlapButton.Size = new System.Drawing.Size(400, 24);
+            this.tlapButton.TabIndex = 2;
+            this.tlapButton.Text = "Load 3-Lap Ghost";
+            this.tlapButton.Click += new System.EventHandler(this.threeLapButton_Click);
+
+            //Label
+            this.label1 = new Label();
+            this.label1.Location = new System.Drawing.Point(8, 224);
+            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Test Label";
+
+            //Others?
+            
+
+            //Combine all
+            this.ClientSize = new System.Drawing.Size(800, 273);
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {
+                        this.flapButton,
+                        this.tlapButton,
+                        this.label1,
+                        });
+            this.Text = "Parent text?";
+
+        }
+
+        private void flapButton_Click(object sender, System.EventArgs e)
+        {
+            type_result = "flap";
+            this.Close();
+        }
+        private void threeLapButton_Click(object sender, System.EventArgs e)
+        {
+            type_result = "3lap";
+            this.Close();
+        }
+    }
+}
--- main.cs
+++ main.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using SocketServer;
 using GhostDownload;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using Win32Form1Namespace;
 
 namespace MainSpace
 {
     class RunProgram
     {
-        public static void tempMain(string[] args)
-        {
-
-        }
         public static void Main(string[] args)
+        {
+            //Create the input dialog box with the parameters below
+            //string input = Interaction.InputBox("What is at the end of the rainbow?", "Riddle", "...", 10, 10);
+            //System.Windows.Forms.ComboBox comboBox1 = new System.Windows.Forms.ComboBox();
+            Win32Form1 form1 = new Win32Form1();
+            System.Windows.Forms.Application.Run(form1);
+            Console.WriteLine(form1.type_result);
+        }
+        public static void tempMain(string[] args)
         {
             //kick's off the watcher loop
             SynchronousSocketServer mk64socket = new SynchronousSocketServer();
