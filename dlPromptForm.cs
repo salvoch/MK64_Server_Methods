1701699661675e629747ac8a97becc8f971987f9
salvoch <github@salvoch.com>
2022-08-21 23:45:27 -0600

    Center text of form.
    This was done by invoking the designer and having it do its own thing

--- dlPromptForm.cs
+++ dlPromptForm.cs
@@ -27,46 +27,60 @@ namespace Win32Form1Namespace
 
         private void InitializeComponent()
         {
-            //Fast lap button
-            this.flapButton = new Button();
+            this.flapButton = new System.Windows.Forms.Button();
+            this.tlapButton = new System.Windows.Forms.Button();
+            this.nolapButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flapButton
+            // 
             this.flapButton.Location = new System.Drawing.Point(10, 50);
+            this.flapButton.Name = "flapButton";
             this.flapButton.Size = new System.Drawing.Size(100, 24);
             this.flapButton.TabIndex = 1;
             this.flapButton.Text = "Fast Lap Ghost";
             this.flapButton.Click += new System.EventHandler(this.flapButton_Click);
-
-            //three lap button
-            this.tlapButton = new Button();
+            // 
+            // tlapButton
+            // 
             this.tlapButton.Location = new System.Drawing.Point(120, 50);
+            this.tlapButton.Name = "tlapButton";
             this.tlapButton.Size = new System.Drawing.Size(100, 24);
             this.tlapButton.TabIndex = 2;
             this.tlapButton.Text = "3-Lap Ghost";
             this.tlapButton.Click += new System.EventHandler(this.threeLapButton_Click);
-
-            //None lap button
-            this.nolapButton = new Button();
+            // 
+            // nolapButton
+            // 
             this.nolapButton.Location = new System.Drawing.Point(65, 85);
+            this.nolapButton.Name = "nolapButton";
             this.nolapButton.Size = new System.Drawing.Size(100, 24);
             this.nolapButton.TabIndex = 3;
             this.nolapButton.Text = "None";
             this.nolapButton.Click += new System.EventHandler(this.noLapButton_Click);
-
-            //Label
-            this.label1 = new Label();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(200, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select which type of ghost you would like to load";
-
-            //Combine all
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Win32Form1
+            // 
             this.ClientSize = new System.Drawing.Size(230, 120);
-            this.Controls.AddRange(new System.Windows.Forms.Control[] {
-                        this.flapButton,
-                        this.tlapButton,
-                        this.nolapButton,
-                        this.label1,
-                        });
+            this.Controls.Add(this.flapButton);
+            this.Controls.Add(this.tlapButton);
+            this.Controls.Add(this.nolapButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Win32Form1";
             this.Text = "Ghost type?";
+            this.ResumeLayout(false);
 
         }
 
@@ -83,6 +97,11 @@ namespace Win32Form1Namespace
         private void noLapButton_Click(object sender, System.EventArgs e)
         {
             this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
