d6768ca1687c6f6d88d13d6a16c89a467b8a501f
salvoch <github@salvoch.com>
2022-08-21 23:25:05 -0600

    Finalize form sizing and display
    Now display all 3 buttons, flap, 3lap, none

--- dlPromptForm.cs
+++ dlPromptForm.cs
@@ -17,6 +17,7 @@ namespace Win32Form1Namespace
         public string type_result = "NA";
         private Button flapButton;
         private Button tlapButton;
+        private Button nolapButton;
         private Label label1;
 
         public Win32Form1()
@@ -24,48 +25,48 @@ namespace Win32Form1Namespace
             this.InitializeComponent();
         }
 
-        [System.STAThreadAttribute()]
-        public static void tMain()
-        {
-            System.Windows.Forms.Application.Run(new Win32Form1());
-        }
-
         private void InitializeComponent()
         {
             //Fast lap button
             this.flapButton = new Button();
-            this.flapButton.Location = new System.Drawing.Point(248, 32);
-            this.flapButton.Size = new System.Drawing.Size(400, 24);
+            this.flapButton.Location = new System.Drawing.Point(10, 50);
+            this.flapButton.Size = new System.Drawing.Size(100, 24);
             this.flapButton.TabIndex = 1;
-            this.flapButton.Text = "Load Fast Lap Ghost";
+            this.flapButton.Text = "Fast Lap Ghost";
             this.flapButton.Click += new System.EventHandler(this.flapButton_Click);
 
             //three lap button
             this.tlapButton = new Button();
-            this.tlapButton.Location = new System.Drawing.Point(248, 64);
-            this.tlapButton.Size = new System.Drawing.Size(400, 24);
+            this.tlapButton.Location = new System.Drawing.Point(120, 50);
+            this.tlapButton.Size = new System.Drawing.Size(100, 24);
             this.tlapButton.TabIndex = 2;
-            this.tlapButton.Text = "Load 3-Lap Ghost";
+            this.tlapButton.Text = "3-Lap Ghost";
             this.tlapButton.Click += new System.EventHandler(this.threeLapButton_Click);
+
+            //None lap button
+            this.nolapButton = new Button();
+            this.nolapButton.Location = new System.Drawing.Point(65, 85);
+            this.nolapButton.Size = new System.Drawing.Size(100, 24);
+            this.nolapButton.TabIndex = 3;
+            this.nolapButton.Text = "None";
+            this.nolapButton.Click += new System.EventHandler(this.noLapButton_Click);
 
             //Label
             this.label1 = new Label();
-            this.label1.Location = new System.Drawing.Point(8, 224);
-            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Size = new System.Drawing.Size(200, 46);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Test Label";
-
-            //Others?
-            
+            this.label1.Text = "Select which type of ghost you would like to load";
 
             //Combine all
-            this.ClientSize = new System.Drawing.Size(800, 273);
+            this.ClientSize = new System.Drawing.Size(230, 120);
             this.Controls.AddRange(new System.Windows.Forms.Control[] {
                         this.flapButton,
                         this.tlapButton,
+                        this.nolapButton,
                         this.label1,
                         });
-            this.Text = "Parent text?";
+            this.Text = "Ghost type?";
 
         }
 
@@ -77,6 +78,10 @@ namespace Win32Form1Namespace
         private void threeLapButton_Click(object sender, System.EventArgs e)
         {
             type_result = "3lap";
+            this.Close();
+        }
+        private void noLapButton_Click(object sender, System.EventArgs e)
+        {
             this.Close();
         }
     }
